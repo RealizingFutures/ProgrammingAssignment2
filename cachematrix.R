8f7f56bad6c429fec0b4c732bd37c5bc52a7884e
RealizingFutures <realizingresonance@gmail.com>
2015-04-18 18:41:10 -0700

    modify cachematrix.R - added remarks

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions that create a special object that stores
+## a numeric matrix and caches its inverse using the solve() function.
+## The function assumes that the matrix supplied is invertible.
 
-## Write a short comment describing this function
+## This function creates a special object that stores a numeric matrix
+## and can also store the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This funtion checks to see if the inverse of this matrix is already
+## cached and if not it calculates the inverse and caches it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
